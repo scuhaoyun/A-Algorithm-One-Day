cc5d7dd668ad9a9eb74bb575fe027ba177686c28
scuhaoyun <1227037748@qq.com>
2016-05-25 16:28:42 +0800

    change algorithm annotation

---
+++ MergeSort/MergeSort/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MergeSort
+//
+//  Created by 郝赟 on 16/5/25.
+//  Copyright © 2016年 haoyun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
